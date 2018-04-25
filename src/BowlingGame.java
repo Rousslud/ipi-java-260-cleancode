83b30f6426909c0cfd157b00981cd9180550faab
rousslud <rousselot.ludovic@yahoo.fr>
2018-04-25 14:11:39 +0200

    Bowling game

--- src/BowlingGame.java
+++ src/BowlingGame.java
@@ -1,12 +1,148 @@
 public class BowlingGame {
 
     private int score;
-
+    private int rollsToPlay=20; 
+    private int framesToPlay=10;
+    private int totalNbStrikes=0; 
+    private int totalNbSpares=0;
+    private int totalNbFails=0;
+    private int totalNbFrames=0;
+    private int totalNbRolls=0;
+    private int roll1Score=0;
+    private int strikeBonusCountRoll=0;
+    private int checkIfNotWrong=1;
+    private boolean frameIn2Rolls=false;
+    private boolean alertCheater=false;
+    private boolean isSpare=false;
+    private boolean isStrike=false;
+    private boolean hasWonExtraRoll=false;
+    private boolean hasWonExtraFrame=false;
+         
     public int score() {
         return score;
-    }
-
+    }   
+    
+    public int nbObSpares() {
+        return totalNbSpares;
+    } 
+    
+    public int nbOfStrikes() {
+        return totalNbStrikes;
+    } 
+    
+    public int nbOfFails() {
+        return totalNbFails;
+    } 
+    
+    public int nbOfRolls() {
+        return totalNbRolls;
+    } 
+    
+    public int nbOfFrames() {
+        return totalNbFrames;
+    } 
+    
+    public int checkIfNotWrong() {
+        return checkIfNotWrong;
+    } 
+    
+    public boolean checkIfCheater() {
+        return alertCheater;
+    } 
+    
+    public boolean checkIfHasWonExtraRoll() {
+        return hasWonExtraRoll;
+    } 
+    
+    public boolean checkIfHasWonExtraFrame() {
+        return hasWonExtraFrame;
+    } 
+      
     public void roll(int numberPins) {
-        score += numberPins;
+    	if (numberPins == 10) {
+    		if (isStrike==true && strikeBonusCountRoll>2) {
+    			strikeBonusCountRoll-=2;
+    			score+=numberPins;
+    			score+=numberPins;
+    		}
+    		else if (isStrike==true && strikeBonusCountRoll<=2 && strikeBonusCountRoll>0) {
+    			strikeBonusCountRoll--;
+    			score+=numberPins;
+    		}
+    		if (isSpare==true) {
+    			isSpare=false;
+    			score+=numberPins;
+    		}
+    		framesToPlay--; 
+    		rollsToPlay-=2;
+    		isStrike=true;
+    		totalNbStrikes++;
+    		totalNbFrames++;
+    		totalNbRolls++;
+    		score+=10;
+    		strikeBonusCountRoll+=2;
+			if (hasWonExtraFrame!= true && framesToPlay==0 && rollsToPlay==0 && isStrike==true) {
+		    	rollsToPlay+=2;
+		    	framesToPlay++;
+		    	hasWonExtraFrame=true;
+		    }
+    	}  
+    	else {
+    		if (isStrike==true && strikeBonusCountRoll>2 && hasWonExtraFrame!=true) {
+    			strikeBonusCountRoll-=2;
+    			score+=numberPins;
+    			score+=numberPins;
+    		}
+    		else if (isStrike==true && strikeBonusCountRoll>2 && hasWonExtraFrame==true) {
+    			strikeBonusCountRoll--;
+    			score+=numberPins;
+    		}
+    		else if ((isStrike==true && strikeBonusCountRoll<=2 && strikeBonusCountRoll>0 && hasWonExtraFrame!=true)) {
+    			strikeBonusCountRoll--;
+    			score+=numberPins;
+    		}
+    		else {
+    			isStrike=false;
+    		}
+    		if (isSpare==true) {
+    			isSpare=false;
+    			score+=numberPins;
+    		}
+    		if (roll1Score+numberPins==10) {
+    			isSpare=true;
+    			totalNbSpares++;
+    		}
+    		if (numberPins == 0) {
+    			totalNbFails++;
+    		}
+    		score+=numberPins;
+    		totalNbRolls++;
+    		rollsToPlay--;
+    		if (frameIn2Rolls==false) {
+    			frameIn2Rolls=true; 
+    			roll1Score=numberPins;
+    		}
+    		else {
+    			frameIn2Rolls=false; 
+    			totalNbFrames++;
+    			framesToPlay--;
+    			roll1Score=0;
+    		}
+    	}
+    	// Allow to add one or two extra rolls if game is finished by a spare or a strike
+    	if (framesToPlay==0 && rollsToPlay==0 && isSpare==true && hasWonExtraFrame!=true) {
+    		rollsToPlay++;
+    		hasWonExtraRoll=true;
+    	}
+    	// Allow to check if we did something wrong. If something is wrong, we will get a negative number for checkIfNotWrong
+    	if (framesToPlay == 0 && rollsToPlay==0) {
+    		checkIfNotWrong--;
+    	}
+    	else if (rollsToPlay<0) {
+    		checkIfNotWrong--;
+    		alertCheater=true;
+    	}	
     }
+    
+
 }
--- src/BowlingGameTest.java
+++ src/BowlingGameTest.java
@@ -7,31 +7,396 @@ public class BowlingGameTest {
 
     BowlingGame game = new BowlingGame();
 
+    private void rollMany(int numberRolls, int numberPins) {
+        for (int i = 0; i < numberRolls; i++) {
+            game.roll(numberPins);
+        }
+    }
+    
     @Test
-    public void queDesGoutieresDonne0Points() {
+    public void QueDesGoutieresDonne0Points() {
         rollMany(20, 0);
         assertEquals(0, game.score());
+        assertEquals(0, game.nbObSpares());
+        assertEquals(0, game.nbOfStrikes());
+        assertEquals(20, game.nbOfFails());
+        assertEquals(20, game.nbOfRolls());
+        assertEquals(10, game.nbOfFrames());
+        assertEquals(0, game.checkIfNotWrong());
+        assertEquals(false, game.checkIfCheater());
+        assertEquals(false, game.checkIfHasWonExtraRoll());
+        assertEquals(false, game.checkIfHasWonExtraFrame());
     }
-
+    
     @Test
-    public void uneSeuleQuille(){
+    public void UneSeuleQuille(){
         game.roll(1);
         rollMany(19, 0);
+        //assertEquals(20, game.totalRollNb());
         assertEquals(1, game.score());
+        assertEquals(0, game.nbObSpares());
+        assertEquals(0, game.nbOfStrikes());
+        assertEquals(19, game.nbOfFails());
+        assertEquals(20, game.nbOfRolls());
+        assertEquals(10, game.nbOfFrames());
+        assertEquals(0, game.checkIfNotWrong());
+        assertEquals(false, game.checkIfCheater());
+        assertEquals(false, game.checkIfHasWonExtraRoll());
+        assertEquals(false, game.checkIfHasWonExtraFrame());
     }
-
+    
     @Test
-    public void bonusDuSpare(){
+    public void QueDesUneSeuleQuille(){
+        rollMany(20, 1);
+        //assertEquals(20, game.totalRollNb());
+        assertEquals(20, game.score());
+        assertEquals(0, game.nbObSpares());
+        assertEquals(0, game.nbOfStrikes());
+        assertEquals(0, game.nbOfFails());
+        assertEquals(20, game.nbOfRolls());
+        assertEquals(10, game.nbOfFrames());
+        assertEquals(0, game.checkIfNotWrong());
+        assertEquals(false, game.checkIfCheater());
+        assertEquals(false, game.checkIfHasWonExtraRoll());
+        assertEquals(false, game.checkIfHasWonExtraFrame());
+    }
+    
+    @Test
+    public void TricheEtFaitUnLancerDeTrop(){
+        rollMany(21, 1);
+        //assertEquals(20, game.totalRollNb());
+        assertEquals(21, game.score());
+        assertEquals(0, game.nbObSpares());
+        assertEquals(0, game.nbOfStrikes());
+        assertEquals(0, game.nbOfFails());
+        assertEquals(21, game.nbOfRolls());
+        assertEquals(10, game.nbOfFrames());
+        assertEquals(-1, game.checkIfNotWrong());
+        assertEquals(true, game.checkIfCheater());
+        assertEquals(false, game.checkIfHasWonExtraRoll());
+        assertEquals(false, game.checkIfHasWonExtraFrame());
+    }
+    
+    @Test
+    public void TricheEtFaitDeuxLancersDeTrop(){
+        rollMany(22, 1);
+        //assertEquals(20, game.totalRollNb());
+        assertEquals(22, game.score());
+        assertEquals(0, game.nbObSpares());
+        assertEquals(0, game.nbOfStrikes());
+        assertEquals(0, game.nbOfFails());
+        assertEquals(22, game.nbOfRolls());
+        assertEquals(11, game.nbOfFrames());
+        assertEquals(-2, game.checkIfNotWrong());
+        assertEquals(true, game.checkIfCheater());
+        assertEquals(false, game.checkIfHasWonExtraRoll());
+        assertEquals(false, game.checkIfHasWonExtraFrame());
+    }
+    
+    @Test
+    public void BonusDunSpare(){
         game.roll(8);
         game.roll(2);
         game.roll(1);
         rollMany(17, 0);
         assertEquals(12, game.score());
+        assertEquals(1, game.nbObSpares());
+        assertEquals(0, game.nbOfStrikes());
+        assertEquals(17, game.nbOfFails());
+        assertEquals(20, game.nbOfRolls());
+        assertEquals(10, game.nbOfFrames());
+        assertEquals(0, game.checkIfNotWrong());
+        assertEquals(false, game.checkIfCheater());
+        assertEquals(false, game.checkIfHasWonExtraRoll());
+        assertEquals(false, game.checkIfHasWonExtraFrame());
     }
-
-    private void rollMany(int numberRolls, int numberPins) {
-        for (int i = 0; i < numberRolls; i++) {
-            game.roll(numberPins);
-        }
+    
+    @Test
+    public void BonusDeuxSparesConsecutifs(){
+        game.roll(8);
+        game.roll(2);
+        game.roll(8);
+        game.roll(2);
+        game.roll(1);
+        game.roll(1);
+        rollMany(14, 0);
+        assertEquals(31, game.score());
+        assertEquals(2, game.nbObSpares());
+        assertEquals(0, game.nbOfStrikes());
+        assertEquals(14, game.nbOfFails());
+        assertEquals(20, game.nbOfRolls());
+        assertEquals(10, game.nbOfFrames());
+        assertEquals(0, game.checkIfNotWrong());
+        assertEquals(false, game.checkIfCheater());
+        assertEquals(false, game.checkIfHasWonExtraRoll());
+        assertEquals(false, game.checkIfHasWonExtraFrame());
+    }
+    
+    @Test
+    public void FiniParSpare(){
+        rollMany(18, 0);
+        game.roll(8);
+        game.roll(2);
+        game.roll(1);
+        assertEquals(12, game.score());
+        assertEquals(1, game.nbObSpares());
+        assertEquals(0, game.nbOfStrikes());
+        assertEquals(18, game.nbOfFails());
+        assertEquals(21, game.nbOfRolls());
+        assertEquals(10, game.nbOfFrames());
+        assertEquals(0, game.checkIfNotWrong());
+        assertEquals(false, game.checkIfCheater());
+        assertEquals(true, game.checkIfHasWonExtraRoll());
+        assertEquals(false, game.checkIfHasWonExtraFrame());
+    }
+    
+    @Test
+    public void BonusDunStrike(){
+        game.roll(10);
+        game.roll(3);
+        game.roll(4);
+        rollMany(16, 0);
+        assertEquals(24, game.score());
+        assertEquals(0, game.nbObSpares());
+        assertEquals(1, game.nbOfStrikes());
+        assertEquals(16, game.nbOfFails());
+        assertEquals(19, game.nbOfRolls());
+        assertEquals(10, game.nbOfFrames());
+        //assertEquals(0, game.checkIfNotWrong());
+        //assertEquals(false, game.checkIfCheater());
+        assertEquals(false, game.checkIfHasWonExtraRoll());
+        assertEquals(false, game.checkIfHasWonExtraFrame());
+    }
+    
+    @Test
+    public void BonusDeuxStrikesConsecutifs(){
+        game.roll(10);
+        game.roll(10);
+        game.roll(4);
+        game.roll(2);
+        game.roll(4);
+        game.roll(4);
+        rollMany(12, 0);
+        // score from https://en.wikipedia.org/wiki/Strike_(bowling)
+        assertEquals(54, game.score());
+        assertEquals(0, game.nbObSpares());
+        assertEquals(2, game.nbOfStrikes());
+        assertEquals(12, game.nbOfFails());
+        assertEquals(18, game.nbOfRolls());
+        assertEquals(10, game.nbOfFrames());
+        assertEquals(0, game.checkIfNotWrong());
+        assertEquals(false, game.checkIfCheater());
+        assertEquals(false, game.checkIfHasWonExtraRoll());
+        assertEquals(false, game.checkIfHasWonExtraFrame());
+    }
+    
+    @Test
+    public void FiniParStrike(){
+        rollMany(18, 0);
+        game.roll(10);
+        game.roll(2);
+        game.roll(1);
+        assertEquals(13, game.score());
+        assertEquals(0, game.nbObSpares());
+        assertEquals(1, game.nbOfStrikes());
+        assertEquals(18, game.nbOfFails());
+        assertEquals(21, game.nbOfRolls());
+        assertEquals(11, game.nbOfFrames());
+        assertEquals(0, game.checkIfNotWrong());
+        assertEquals(false, game.checkIfCheater());
+        assertEquals(false, game.checkIfHasWonExtraRoll());
+        assertEquals(true, game.checkIfHasWonExtraFrame());
+    }
+    
+    @Test
+    public void QueDesStrikes(){
+        rollMany(11, 10);
+        assertEquals(300, game.score());
+        assertEquals(0, game.nbObSpares());
+        assertEquals(11, game.nbOfStrikes());
+        assertEquals(0, game.nbOfFails());
+        assertEquals(11, game.nbOfRolls());
+        assertEquals(11, game.nbOfFrames());
+        assertEquals(0, game.checkIfNotWrong());
+        assertEquals(false, game.checkIfCheater());
+        assertEquals(false, game.checkIfHasWonExtraRoll());
+        assertEquals(true, game.checkIfHasWonExtraFrame());
+    }
+     
+    @Test
+    public void SpareSuiviDunStrike(){
+    	game.roll(9);
+    	game.roll(1);
+    	game.roll(10);
+        rollMany(2, 2);
+        rollMany(14, 0);
+        assertEquals(38, game.score());
+        assertEquals(1, game.nbObSpares());
+        assertEquals(1, game.nbOfStrikes());
+        assertEquals(14, game.nbOfFails());
+        assertEquals(19, game.nbOfRolls());
+        assertEquals(10, game.nbOfFrames());
+        assertEquals(0, game.checkIfNotWrong());
+        assertEquals(false, game.checkIfCheater());
+        assertEquals(false, game.checkIfHasWonExtraRoll());
+        assertEquals(false, game.checkIfHasWonExtraFrame());
+    }
+    
+    @Test
+    public void StrikeSuiviDunSpare(){
+    	game.roll(10);
+    	game.roll(9);
+    	game.roll(1);
+        rollMany(16, 2);
+        assertEquals(64, game.score());
+        assertEquals(1, game.nbObSpares());
+        assertEquals(1, game.nbOfStrikes());
+        assertEquals(0, game.nbOfFails());
+        assertEquals(19, game.nbOfRolls());
+        assertEquals(10, game.nbOfFrames());
+        assertEquals(0, game.checkIfNotWrong());
+        assertEquals(false, game.checkIfCheater());
+        assertEquals(false, game.checkIfHasWonExtraRoll());
+        assertEquals(false, game.checkIfHasWonExtraFrame());
+    }
+    
+    @Test
+    public void StrikeSpareStrike(){
+    	game.roll(10);
+    	game.roll(9);
+    	game.roll(1);
+    	game.roll(10);
+        rollMany(2, 2);
+        rollMany(12, 0);
+        assertEquals(58, game.score());
+        assertEquals(1, game.nbObSpares());
+        assertEquals(2, game.nbOfStrikes());
+        assertEquals(12, game.nbOfFails());
+        assertEquals(18, game.nbOfRolls());
+        assertEquals(10, game.nbOfFrames());
+        assertEquals(0, game.checkIfNotWrong());
+        assertEquals(false, game.checkIfCheater());
+        assertEquals(false, game.checkIfHasWonExtraRoll());
+        assertEquals(false, game.checkIfHasWonExtraFrame());
+    }
+    
+    @Test
+    public void ReproduireUnVraiScore1() {
+    	// Use: http://forum.brice.free.fr/index.php?section=regles
+        game.roll(7);
+        game.roll(2);
+        game.roll(8);
+        game.roll(2);
+        game.roll(5);
+        game.roll(4);
+        game.roll(10);
+        game.roll(8);
+        game.roll(0);
+        game.roll(6);
+        game.roll(0);
+        game.roll(8);
+        game.roll(1);
+        game.roll(10);
+        game.roll(9);
+        game.roll(1);
+        game.roll(10);
+        game.roll(7);
+        game.roll(0);
+        assertEquals(131, game.score());
+        assertEquals(2, game.nbObSpares());
+        assertEquals(3, game.nbOfStrikes());
+        assertEquals(3, game.nbOfFails());
+        assertEquals(19, game.nbOfRolls());
+        assertEquals(11, game.nbOfFrames());
+        assertEquals(0, game.checkIfNotWrong());
+        assertEquals(false, game.checkIfCheater());
+        assertEquals(false, game.checkIfHasWonExtraRoll());
+        assertEquals(true, game.checkIfHasWonExtraFrame());
+    }
+   
+    
+    @Test
+    public void StrikeSuiviDeuxSpares(){
+    	game.roll(10);
+    	game.roll(7);
+    	game.roll(3);
+    	game.roll(5);
+    	game.roll(5);
+        rollMany(2, 2);
+        rollMany(12, 0);
+        assertEquals(51, game.score());
+        assertEquals(2, game.nbObSpares());
+        assertEquals(1, game.nbOfStrikes());
+        assertEquals(12, game.nbOfFails());
+        assertEquals(19, game.nbOfRolls());
+        assertEquals(10, game.nbOfFrames());
+        assertEquals(0, game.checkIfNotWrong());
+        assertEquals(false, game.checkIfCheater());
+        assertEquals(false, game.checkIfHasWonExtraRoll());
+        assertEquals(false, game.checkIfHasWonExtraFrame());
+    }
+    
+    @Test
+    public void ReproduireUnVraiScore2() {
+    	// Use: https://www.tutorialspoint.com/ten_pin_bowling/ten_pin_bowling_scoring.htm == took the line of DH
+        game.roll(10);
+        game.roll(8);
+        game.roll(2);
+        game.roll(9);
+        game.roll(1);
+        game.roll(8);
+        game.roll(0);
+        game.roll(10);
+        game.roll(10);
+        game.roll(9);
+        game.roll(1);
+        game.roll(9);
+        game.roll(1);
+        game.roll(10);
+        game.roll(10);
+        game.roll(9);
+        game.roll(1);
+        assertEquals(202, game.score());
+        assertEquals(5, game.nbObSpares());
+        assertEquals(5, game.nbOfStrikes());
+        assertEquals(1, game.nbOfFails());
+        assertEquals(17, game.nbOfRolls());
+        assertEquals(11, game.nbOfFrames());
+        assertEquals(0, game.checkIfNotWrong());
+        assertEquals(false, game.checkIfCheater());
+        assertEquals(false, game.checkIfHasWonExtraRoll());
+        assertEquals(true, game.checkIfHasWonExtraFrame());
+    }
+    
+    @Test
+    public void ReproduireUnVraiScore3() {
+    	// Use: https://www.tutorialspoint.com/ten_pin_bowling/ten_pin_bowling_scoring.htm == took the line of DT
+        game.roll(7);
+        game.roll(3);
+        game.roll(10);
+        game.roll(10);
+        game.roll(8);
+        game.roll(1);
+        game.roll(9);
+        game.roll(1);
+        game.roll(8);
+        game.roll(1);
+        game.roll(10);
+        game.roll(9);
+        game.roll(1);
+        game.roll(8);
+        game.roll(2);
+        game.roll(6);
+        game.roll(1);
+        assertEquals(164, game.score());
+        assertEquals(4, game.nbObSpares());
+        assertEquals(3, game.nbOfStrikes());
+        assertEquals(0, game.nbOfFails());
+        assertEquals(17, game.nbOfRolls());
+        assertEquals(10, game.nbOfFrames());
+        assertEquals(0, game.checkIfNotWrong());
+        assertEquals(false, game.checkIfCheater());
+        assertEquals(false, game.checkIfHasWonExtraRoll());
+        assertEquals(false, game.checkIfHasWonExtraFrame());
     }
 }
